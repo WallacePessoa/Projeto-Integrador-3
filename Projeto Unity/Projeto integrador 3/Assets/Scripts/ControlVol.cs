1120333926af00a0acc762922a2a99201b89adc0
camilafacens <54379899+camilafacens@users.noreply.github.com>
2020-04-26 17:20:29 -0300

    Options Scene (Volum, Screen, Tutorial)

--- Projeto Unity/Projeto integrador 3/Assets/Scripts/ControlVol.cs
+++ Projeto Unity/Projeto integrador 3/Assets/Scripts/ControlVol.cs
@@ -6,14 +6,14 @@ using UnityEngine.UI;
 public class ControlVol : MonoBehaviour
 {
     public float volumMaster, volumFX, volumMusic;
-    public Slider sliderMaster, sliderFX, sliderMusic;
+    //public Slider sliderMaster, sliderFX, sliderMusic;
 
     // Start is called before the first frame update
     void Start()
     {
-        sliderMaster.value = PlayerPrefs.GetFloat("Master");
-        sliderFX.value = PlayerPrefs.GetFloat("FX");
-        sliderMusic.value = PlayerPrefs.GetFloat("Music");
+        //sliderMaster.value = PlayerPrefs.GetFloat("Master");
+        //sliderFX.value = PlayerPrefs.GetFloat("FX");
+        //sliderMusic.value = PlayerPrefs.GetFloat("Music");
     }
 
     // Update is called once per frame
@@ -27,7 +27,7 @@ public class ControlVol : MonoBehaviour
         volumMaster = volume;
         AudioListener.volume = volumMaster;
 
-        PlayerPrefs.SetFloat("Master", volumMaster);
+        //PlayerPrefs.SetFloat("Master", volumMaster);
     }
 
     public void VolumFX(float volume)
@@ -40,7 +40,7 @@ public class ControlVol : MonoBehaviour
             Fxs[i].GetComponent<AudioSource>().volume = volumFX;
         }
 
-        PlayerPrefs.SetFloat("FX", volumFX);
+        //PlayerPrefs.SetFloat("FX", volumFX);
     }
 
     public void VolumMusic(float volume)
@@ -53,6 +53,6 @@ public class ControlVol : MonoBehaviour
           Musics[i].GetComponent<AudioSource>().volume = volumMusic;
         }
 
-        PlayerPrefs.SetFloat("Music", volumMusic);
+        //PlayerPrefs.SetFloat("Music", volumMusic);
     }
 }
