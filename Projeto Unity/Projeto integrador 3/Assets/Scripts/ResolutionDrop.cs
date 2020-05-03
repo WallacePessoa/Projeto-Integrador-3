bf43a326005f987defa1dd895150a4147c41e3e3
camilafacens <54379899+camilafacens@users.noreply.github.com>
2020-05-03 19:17:13 -0300

    Cena Screen

---
+++ Projeto Unity/Projeto integrador 3/Assets/Scripts/ResolutionDrop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResolutionDrop : MonoBehaviour
+{
+    Dropdown dropdown;
+    List<Dropdown.OptionData> options = new List<Dropdown.OptionData>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //PlayerPrefs.SetInt("UnitySelectMonitor", 0);
+
+        dropdown = GetComponent<Dropdown>();
+        dropdown.ClearOptions();
+
+        //for(int i = 0; i < Screen.
+     }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
