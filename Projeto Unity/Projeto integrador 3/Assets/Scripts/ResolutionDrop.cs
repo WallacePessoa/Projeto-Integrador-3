8d46f92d2f5446951e8bc88574a64eaf298d90ee
camilafacens <54379899+camilafacens@users.noreply.github.com>
2020-05-07 17:45:40 -0300

    Script screen

--- Projeto Unity/Projeto integrador 3/Assets/Scripts/ResolutionDrop.cs
+++ Projeto Unity/Projeto integrador 3/Assets/Scripts/ResolutionDrop.cs
@@ -16,7 +16,15 @@ public class ResolutionDrop : MonoBehaviour
         dropdown = GetComponent<Dropdown>();
         dropdown.ClearOptions();
 
-        //for(int i = 0; i < Screen.
+        //for(int i = 0; i < Screen.resolutions.Lenght; i++)
+        //{
+        //    Dropdown.OptionData item = new Dropdown.OptionData();
+        //    item.text = Screen.resolutions[i].widht + " x " + Screen.resolutions[i] + " (" + Screen.resolution[i].refreshRate + ")";
+        //    options.Add(item);
+        //}
+
+        dropdown.AddOptions(options);
+        //dropdown.value = PlayerPrefs.GetInt("ResolutionSetting", Screen.resolutions.Lengh);
      }
 
     // Update is called once per frame
